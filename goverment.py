6a3b7c9ad41e8a1fec9902a694dba3d34b63e633
albertmena <alber.mena@gmail.com>
2016-06-21 20:23:26 +0200

    Created and deleted feeds in map_feeds, Added plot of map_feeds --> fix bug ploting both maps (feeds maps have num_feeds dimensions) --> fix bug retiring population

--- goverment.py
+++ goverment.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import threading
+import pylab
+
 
 '''------------GOVERMENT'''
 class Goverment:
@@ -29,10 +31,10 @@ class Map:
      map as 3dim matrix, (row, col, feeds (numfeeds + 1/0 if cell in position)
      cell in position: [N][[N][pos, feed1, feed2, feed3, feed4, feed5]
      '''
-    def __init__(self,  size, numFeeds):
+    def __init__(self,  size, num_feeds):
         self.size = size
-        self.numFeeds = numFeeds
-        self.map_feeds = np.zeros((self.size,  self.size))#incluye recusros de celda
+        self.num_feeds = num_feeds
+        self.map_feeds = np.zeros((self.num_feeds, self.size,  self.size))#incluye recusros de celda
         self.map_cells = np.zeros((self.size,  self.size)) #ncluye posicion celula
 
     def available(self, position):
@@ -56,31 +58,36 @@ class Map:
             return False
 
     def ploting(self):
+        pylab.axis([0, self.size, 0, self.size])
+        pylab.ion()  # in order to enable interactive plotting
+        while True:
+            pylab.matshow(self.map_cells, fignum=1, cmap=plt.cm.gray)
+            for i in range(0,self.num_feeds):
+                pylab.title('feed {}'.format(i))
+                pylab.matshow(self.map_feeds[i][:][:], fignum=i, cmap=plt.cm.gray)
+            pylab.pause(2)
 
-        plt.axis([0, self.size, 0, self.size])
-        plt.ion() #in order to enable interactive plotting
-        for i in range(20):
-            plt.matshow(self.map_cells, fignum=1, cmap=plt.cm.gray)
-            plt.pause(1)
 
 
 '''------------NATURE'''
 class Nature:
     '''manage feed seeds, delete feeds (eat by cells)'''
-    def __init__(self, abundance):
+    def __init__(self, abundance, num_feeds):
         self.abundance = abundance
-        self.num_feeds = 5
+        self.num_feeds = num_feeds
         self.feeds = 0
 
-    def initialSeed(selfself):
-        pass
+    def initialSeed(self, map_i):
+        map_size = map_i.map_feeds.shape
+        map_i.map_feeds = np.random.randint(0, self.abundance, size = map_size)
 
-    def deleteFeed(self, position, feeds):
-        pass
+    def deleteFeed(self, position, feed):
+        map_i.map_feeds[feed][position[0]][position[1]] =\
+            map_i.map_feeds[feed][position[0]][position[1]] - 1#lo ultimo la columna siempre
 
-    def createFeed(self):
-        pass
-
+    def createFeed(self, position, feed):
+        map_i.map_feeds[feed][position[0]][position[1]] = \
+            map_i.map_feeds[feed][position[0]][position[1]] + 1
 
 '''------------CELLS'''
 class MotherCell:
@@ -165,23 +172,22 @@ class MotherCell:
 '''-----------MAIN'''
 if __name__ == '__main__':
     goverment_i = Goverment()
-    nature_i = Nature(5)
-    map_i = Map(100, nature_i.num_feeds)
+    num_feeds = 3
+    map_i = Map(100, num_feeds)
+    nature_i = Nature(10, num_feeds)
+    nature_i.initialSeed(map_i)
     goverment_i.createPopulation((2,2))
-    t = threading.Thread(target=map_i.ploting)
+    t_map = threading.Thread(target=map_i.ploting)
     print "Iniciada la vida"
     print "Cell position: ", goverment_i.listCells[0].position
-    t.start()
+    t_map.start()
     time.sleep(1)
     goverment_i.listCells[0].smell()
-    print "Cell position: ", goverment_i.listCells[0].position
     time.sleep(1)
     goverment_i.listCells[0].smell()
-    print "Cell position: ", goverment_i.listCells[0].position
     time.sleep(1)
     goverment_i.listCells[0].smell()
-    print "Cell position: ", goverment_i.listCells[0].position
     time.sleep(1)
     goverment_i.listCells[0].smell()
-    print "Cell position: ", goverment_i.listCells[0].position
+
     goverment_i.retirePopulation(goverment_i.listID[0].ID)
